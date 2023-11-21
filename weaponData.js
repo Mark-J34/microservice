4626e3c2a2341611c2b81eb0f1b3d436edb9b10c
MarkJ3423 <jensen342344@gmail.com>
2023-11-20 16:42:12 -0800

    added data

---
+++ weaponData.js
@@ -0,0 +1,9 @@
+const items = [
+    {"numerical_id": 1, "id": "Diamond Sword", "damage": 7, "durability": 1561},
+    {"numerical_id": 2, "id": "Wood Sword", "damage": 4, "durability": 59},
+    {"numerical_id": 3, "id": "Wood Axe", "damage": 3, "durability": 59},
+    {"numerical_id": 4, "id": "Gold Axe", "damage": 3, "durability": 32},
+    {"numerical_id": 5, "id": "Gold Sword", "damage": 4, "durability": 32}
+  ];
+  
+  module.exports = items;
